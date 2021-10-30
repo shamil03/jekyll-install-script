76f1e646b2130b5ad6c72b8217dc37ad74318c91
shamil03 <62066346+shamil03@users.noreply.github.com>
2021-10-30 21:27:27 +0530

    Implement Debian installer script (#5)

---
+++ debian.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt-get install ruby-full build-essential gcc g++ make
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+gem install jekyll bundler
