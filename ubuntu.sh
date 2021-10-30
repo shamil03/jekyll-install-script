c8514e9b6c24b59622043b9576ce545662ee6efd
shamil03 <62066346+shamil03@users.noreply.github.com>
2021-10-30 17:15:59 +0530

    Implement Ubuntu installer script (#1)

---
+++ ubuntu.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt-get install ruby-full build-essential zlib1g-dev gcc g++ make 
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+gem install jekyll bundler
