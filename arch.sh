dc68e8233b96cfe53934d95812024226240313ac
shamil <shamilpeiris03@gmail.com>
2021-10-31 08:03:30 +0530

    Implement Arch installer script

--- arch.sh
+++ arch.sh
@@ -1,5 +1,5 @@
-sudo apt update
-sudo apt-get install ruby-full build-essential zlib1g-dev gcc g++ make 
+sudo pacman -Syy
+sudo pacman -S ruby base-devel gcc cmake
 echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
